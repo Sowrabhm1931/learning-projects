77efe5f931807ca8c2f915bb4b23cc3aa0c20551
Sowrabh Manjapp <mr.sowrabh.m@gmail.com>
2024-06-06 21:40:27 +0100

    Update for Dockerfile

--- kafka-producer-consumer/producer.py
+++ kafka-producer-consumer/producer.py
@@ -1,6 +1,6 @@
 from confluent_kafka import Producer
 
-conf = {'bootstrap.servers':"localhost:9092"}
+conf = {'bootstrap.servers': "localhost:9092"}
 producer = Producer(**conf)
 
 def delivery_report(err, msg):
@@ -9,7 +9,9 @@ def delivery_report(err, msg):
     else:
         print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
-
-message = 'This is fun to play with!!'
-producer.produce('orders', value=message, callback=delivery_report)
-producer.flush()
+while True:
+    message = input("Enter a message to send (or type 'exit' to quit): ")
+    if message.lower() == 'exit':
+        break
+    producer.produce('orders', value=message, callback=delivery_report)
+    producer.flush()
